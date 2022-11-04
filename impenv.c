310a6e7749234d30e51dfc716f3056dd6e7ae707
Gralav <lavenderwaga@gmail.com>
2022-11-04 07:02:19 -0700

    impenv

--- impenv.c
+++ impenv.c
@@ -9,7 +9,7 @@ char *_impenv(char *env)
 	int i = 0, n = 0;
 	char *temp, *res;
 
-	while (env[i] != NULL)
+	while (environ[i] != NULL)
 	{
 		if (strcmp(environ[i], env) == 0)
 			temp = environ[i];
