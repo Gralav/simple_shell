c64d8eccb89ad25cd20f2ed5bb190f119e1e1740
Gralav <lavenderwaga@gmail.com>
2022-11-04 05:32:31 -0700

    5. Simple shell 1.0

--- impenv.c
+++ impenv.c
@@ -9,16 +9,16 @@ char *_impenv(char *env)
 	int i = 0, n = 0;
 	char *temp, *res;
 
-	while (environ[i] != NULL)
+	while (env[i] != NULL)
 	{
-		if (_strcmp(environ[i], env) == 0)
+		if (strcmp(environ[i], env) == 0)
 			temp = environ[i];
 		i++;
 	}
 	while (temp[n] != '\0')
 	{
 		if (_strcmp(temp, env) == 0)
-			res = _strstr(temp, "/");
+			res = strstr(temp, "/");
 		n++;
 	}
 	return (res);
