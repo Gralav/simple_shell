cfa78aa70f45450e4479ace67c6808a4aca4b624
Gralav <lavenderwaga@gmail.com>
2022-11-15 05:43:43 -0800

    5. Simple shell 1.0

--- impenv.c
+++ impenv.c
@@ -1,24 +1,26 @@
 #include "shell.h"
 /**
- * _impenv - gets env of input
+ * _getenv - gets env of input
  * @env: input
  * Return: env without =.
  */
-char *_impenv(char *env)
+
+
+char *_getenv(char *env)
 {
 	int i = 0, n = 0;
 	char *temp, *res;
 
 	while (environ[i] != NULL)
 	{
-		if (strcmp(environ[i], env) == 0)
+		if (_strcmp(environ[i], env) == 0)
 			temp = environ[i];
 		i++;
 	}
 	while (temp[n] != '\0')
 	{
-		if (strcmp(temp, env) == 0)
-			res = strstr(temp, "/");
+		if (_strcmp(temp, env) == 0)
+			res = _strstr(temp, "/");
 		n++;
 	}
 	return (res);
